968a5ed74d16323344499bb40f46525b5cd0220c
Miller Rosenthal <millerrosenthal@ucmerced.edu>
2026-04-09 10:25:37 -0700

    Update confirm.ts
    default settings now appear if you dont go to settings first

--- src/confirm.ts
+++ src/confirm.ts
@@ -15,27 +15,35 @@ app.innerHTML = `
 `
 //input gamestate details
 const details = document.getElementById('details') as HTMLElement | null
-const raw = sessionStorage.getItem('gameSettings')
+const settings = sessionStorage.getItem('gameSettings')
 
-if (raw) {
-  try {
-    const s = JSON.parse(raw)
+if (settings) {
+    const s = JSON.parse(settings)
     if (details) {
       details.innerHTML = `
         <p>Gamemode: ${s.gamemode}</p>
         <p>Difficulty: ${s.difficulty}</p>
         <p>Difficulty Growth Speed: ${s.difficultyGrowthSpeed}</p>
       `} 
-  } catch (e) {
-    if (details) details.textContent = '(no saved settings)'
-  }
 } else {
-  if (details) details.textContent = '(no saved settings)'
+    const newSettings = {
+    gamemode: 'standing',
+    difficulty: 'medium',
+    difficultyGrowthSpeed: 0.1,
+  }
+  if (details) {
+      details.innerHTML = `
+        <p>Gamemode: ${newSettings.gamemode}</p>
+        <p>Difficulty: ${newSettings.difficulty}</p>
+        <p>Difficulty Growth Speed: ${newSettings.difficultyGrowthSpeed}</p>
+      `} 
+
+  sessionStorage.setItem('gameSettings', JSON.stringify(newSettings))
+
 }
 
 const back = document.getElementById('back') as HTMLButtonElement | null
 if (back) back.onclick = () => (window.location.href = '/')
-
 
 const start = document.getElementById('start') as HTMLButtonElement
 start.onclick = () => (window.location.href = '/game')
