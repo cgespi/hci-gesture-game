06c807d4eebe697579ad9c041ad086014bf4119d
Miller Rosenthal <millerrosenthal@ucmerced.edu>
2026-04-11 13:11:01 -0700

    Update game.ts
    added a launch path arc that appears when screen is clicked. not a planned feature but more so just a framework for future work where the ball launching animation

--- src/game.ts
+++ src/game.ts
@@ -7,8 +7,18 @@ const addHit = document.createElement('button');
 const addMiss = document.createElement('button');
 const status = document.createElement('div');
 const root = document.createElement('div');
+const ball = {
+  x: 0,
+  y: 0,
+  t: 0,
+};
+
+
+let cannonX = 0;
+let cannonY = 0;
 let hitCount = 0;
 let missCount = 0;
+
 
 root.id = 'gameRoot';
 canvas.id = 'gameCanvas';
@@ -18,7 +28,7 @@ addHit.textContent = 'Add Hit';
 addMiss.textContent = 'Add Miss';
 
 Object.assign(root.style, { position: 'fixed', inset: '0', overflow: 'hidden', background: '#307bc6' });
-Object.assign(canvas.style, { display: 'block', width: '100%', height: '100%' });
+Object.assign(canvas.style, {display: 'block', top: '0', left: '0', width: '100%', height: '100%' });
 Object.assign(exit.style, { position: 'absolute', right: '12px', top: '12px' });
 Object.assign(addHit.style, { position: 'absolute', right: '12px', top: '64px' });
 Object.assign(addMiss.style, { position: 'absolute', right: '12px', top: '120px' });
@@ -59,6 +69,33 @@ canvas.addEventListener('mouseleave', () => { mouse.inside = false; status.textC
 
 function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
+}
+
+function launch(){
+  const arcHeight = 50;
+  ball.t += 0.03;
+  if (ball.t > 1){
+    ball.t = 1;
+    ctx.fillStyle ='#fff732';
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+    return;
+  }
+  
+
+  ball.x = cannonX + (mouse.x - cannonX) * ball.t;
+  const yLinear = cannonY + (mouse.y - cannonY) * ball.t;
+  ball.y = yLinear - arcHeight * (4 * ball.t * (1 - ball.t));
+
+
+  ctx.fillStyle ='#000';
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, 5, 0, Math.PI * 2);
+  ctx.fill();
+
+  if (ball.t < 1) requestAnimationFrame(launch);
+
 }
 
 function drawCannon(){
@@ -105,6 +142,9 @@ function drawCannon(){
     ctx.beginPath();
     ctx.arc(x,y, 15, 0, Math.PI * 2);
     ctx.fill();
+
+    cannonX = x;
+    cannonY = y;
   } 
 }
 
@@ -142,6 +182,11 @@ addMiss.addEventListener('click', () => {
     status.textContent = `Mouse: ${mouse.x}, ${mouse.y}, hitCount: ${hitCount}, missCount: ${missCount}`;
 });
 
+canvas.addEventListener('click', () => {
+  ball.t = 0;
+  launch();
+});
+
 exit.addEventListener('click', () => {
   const stats = {
     hits: hitCount,
@@ -151,4 +196,3 @@ exit.addEventListener('click', () => {
   sessionStorage.setItem('gameStats', JSON.stringify(stats))
     window.location.href = '/postGame';
 });
-
